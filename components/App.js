752e646de48502e3309bdb209ac3c11a9cfa0292
Michael-Bryant Choa <mbchoa@gmail.com>
2016-05-31 13:35:28 -0700

    move event handling logic from SearchBarContainer to main App component

--- components/App.js
+++ components/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import SearchBarContainer from './SearchBarContainer';
+import SearchBar from './SearchBar';
 import RecipeList from './RecipeList';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchInput: '',
+    };
+    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+    this.handleSubmitSearch = this.handleSubmitSearch.bind(this);
+  }
+
+  handleSearchTextChange(e) {
+    this.setState({
+      searchInput: e.target.value,
+    });
+  }
+
+  handleSubmitSearch(e) {
+    e.preventDefault();
+    console.log(`search for: ${this.state.searchInput}`);
   }
 
   render() {
     return (
       <div>
-        <SearchBarContainer />
+        <SearchBar
+          onTextChange={this.handleSearchTextChange}
+          onSubmitSearch={this.handleSubmitSearch} />
         <RecipeList />
       </div>
     );
