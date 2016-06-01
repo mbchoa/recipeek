4c3610a05d13e67223a48c6ceaa39410b3b82215
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-01 11:57:31 -0700

    implement cheerio to scrape each recipe source page htmls body content

--- server/controllers/scraperController.js
+++ server/controllers/scraperController.js
@@ -6,70 +6,24 @@ const getRecipePageHTML = url =>
     request(url, (err, res, html) => {
       if (err) return console.log(`failed to request recipe page @ ${url}`);
       const $ = cheerio.load(html);
-      resolve([]);
+      resolve({
+        body: $('body').text(),
+      });
     });
   });
 
 module.exports = {
   getAllScrapedRecipePages: (req, res, next) => {
     const recipesSourceUrlArr = req.parsedData.recipes.map(recipeData => recipeData.source_url);
-    console.log('recipes source url', recipesSourceUrlArr);
+    const recipesSourceUrlPromisesArr = recipesSourceUrlArr.map(getRecipePageHTML);
+    Promise
+      .all(recipesSourceUrlPromisesArr)
+      .then(recipeHtmlArr => {
+        req.parsedData.recipes = req.parsedData.recipes.map((recipeData, i) => {
+          recipeData.body = recipeHtmlArr[i].body;
+          return recipeData;
+        });
+        next();
+      }).catch(err => console.log('error resolving recipes', err));
   },
-  //   const brandPromise = new Promise((resolve, reject) => {
-  //     request(`https://www.thewhiskyexchange.com/b/40/${req.params.brand}-single-malt-scotch-whisky`, (error, response, html) => {
-  //       let $$ = cheerio.load(html);
-  //       const output = $$('.group-list .product').map((index, elem) => {
-  //         return {
-  //           productName: $$(elem).attr('title'),
-  //           productPrice: $$(elem).children('.price').text(),
-  //         }
-  //       }).toArray();
-  //
-  //       const productPageArr = $$('.group-list .product').map((index, elem) => {
-  //         return $$(elem).attr('href');
-  //       }).toArray();
-  //
-  //       resolve({
-  //         productData: output,
-  //         productPageArr: productPageArr
-  //       });
-  //
-  //     });
-  //   }).then((resolveData) => {
-  //     function getScotchProduct(url) {
-  //       return new Promise((resolve, reject) => {
-  //         request(`https://www.thewhiskyexchange.com${url}`, (err, res, html) => {
-  //           if (err) {
-  //             console.log('error occurred');
-  //           }
-  //           const $ = cheerio.load(html);
-  //           const productFacts = [];
-  //           $('#prodMeta .meta').children().each((index, el) => {
-  //             if (index % 2 === 0) {
-  //               productFacts.push({
-  //                 fact: $(el).text(),
-  //                 value: undefined,
-  //               });
-  //             } else {
-  //               productFacts[productFacts.length - 1].value = $(el).text();
-  //             }
-  //           });
-  //           resolve(productFacts);
-  //         })
-  //       });
-  //     }
-  //     const promisesArr = resolveData.productPageArr.map(url => getScotchProduct(url));
-  //     Promise
-  //       .all(promisesArr)
-  //       .then(dataArr => {
-  //         const output = resolveData.productData.map((data, i) => {
-  //           data.productFacts = dataArr[i];
-  //           return data;
-  //         });
-  //         res.send(output);
-  //       });
-  //   }).catch(err => {
-  //     console.log(err);
-  //   });
-  // }
 };
