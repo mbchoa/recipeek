6244a175b9d93c00002be19dff550b1cc6d978db
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-16 00:53:58 -0700

    setup static file servering middleware, setup compression middleware

--- server/apiServer.js
+++ server/apiServer.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const compression = require('compression');
 const searchController = require('./controllers/searchController');
 const scraperController = require('./controllers/scraperController');
 const keywordFilterController = require('./controllers/keywordFilterController');
 const nlpController = require('./controllers/nlpController');
 
 app.use(cors());
+app.use(compression());
+app.use(express.static(__dirname + '/../public'));
 
 app.get('/search/:ingredient',
   searchController,
