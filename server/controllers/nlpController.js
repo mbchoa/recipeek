3b8e257dd1c64e7a21e031d4eb4cbee831e88921
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-17 20:58:47 -0700

    undo change to instantiate new farm on each call to nlp controller middleware

--- server/controllers/nlpController.js
+++ server/controllers/nlpController.js
@@ -1,18 +1,17 @@
-const workerFarm = require('worker-farm');
+const workerFarm = require('worker-farm')
+  , nlpWorkers = workerFarm({
+      maxCallsPerWorker: 4,
+    },
+    require.resolve('./nlpWorker'));
 var ret = 0;
 
 module.exports = (req, res, next) => {
   console.log('-> nlp controller entry point');
-  var nlpWorkers = workerFarm({
-      maxCallsPerWorker: 4,
-    },
-    require.resolve('./nlpWorker'));
   req.parsedData.forEach((recipeData, i) => {
     nlpWorkers(req.recipeBodyArr[i], function (err, nlpTerms) {
       recipeData.keywords = nlpTerms;
       if (++ret === req.parsedData.length) {
         ret = 0;
-        workerFarm.end(nlpWorkers);
         next();
       }
     });
