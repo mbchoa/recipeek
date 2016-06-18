0bf6ba43e1498112c4087baf252f6423a930706a
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-17 20:53:15 -0700

    instantiate new nlpWorkers on each call of nlpController middleware

--- server/controllers/nlpController.js
+++ server/controllers/nlpController.js
@@ -1,17 +1,18 @@
-const workerFarm = require('worker-farm')
-  , nlpWorkers = workerFarm({
-      maxCallsPerWorker: 4,
-    },
-    require.resolve('./nlpWorker'));
+const workerFarm = require('worker-farm');
 var ret = 0;
 
 module.exports = (req, res, next) => {
   console.log('-> nlp controller entry point');
+  let nlpWorkers = workerFarm({
+      maxCallsPerWorker: 4,
+    },
+    require.resolve('./nlpWorker'));
   req.parsedData.forEach((recipeData, i) => {
     nlpWorkers(req.recipeBodyArr[i], function (err, nlpTerms) {
       recipeData.keywords = nlpTerms;
       if (++ret === req.parsedData.length) {
         ret = 0;
+        workerFarm.end(nlpWorkers);
         next();
       }
     });
