df17c8400cfc74282ca8b7781220dea4cb2a9078
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-20 15:26:57 -0700

    lower max number of recipe results from 30 to 10

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const MAX_NUM_RESULTS = 10;
 
 module.exports = (req, res, next) => {
   console.log('-> search controller entry point');
@@ -7,7 +8,10 @@ module.exports = (req, res, next) => {
   req.start = Date.now();
   request(`http://food2fork.com/api/search?key=23a189635e90b56842dee3b32139deac&q=${req.params.ingredient}`, (err, resp, html) => {
     // console.log('* search request round trip = ', Date.now() - startTime);
-    req.parsedData = JSON.parse(html).recipes;
+    req.parsedData = JSON
+      .parse(html)
+      .recipes
+      .slice(0, MAX_NUM_RESULTS);
     next();
   });
 };
