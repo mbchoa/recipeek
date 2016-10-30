64c07b04861f9aadc7bebdf2dba0c3ca20c33042
Michael-Bryant Choa <mbchoa@gmail.com>
2016-10-30 02:31:45 -0700

    setup WDS to create dev environment with HMR and reat hot reload and proxy to redirect api requests from WDS port 8080 to express api port 3000

--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,29 @@ var webpack = require('webpack');
 var config = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './index.js',
-        'webpack-hot-middleware/client'
     ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: '/dist/',
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/
+                loaders: ['react-hot', 'babel'],
+                exclude: /node_modules/,
             }
         ]
     },
     plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+        new webpack.NoErrorsPlugin(),
+    ],
 };
 
 module.exports = config;
