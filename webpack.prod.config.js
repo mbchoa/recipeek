0fda83e98e743b745627fe30053a6a422695f478
Michael-Bryant Choa <mbchoa@gmail.com>
2016-10-30 03:58:06 -0700

    update scripts, add production build webpack config

---
+++ webpack.prod.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        './index.js',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            comments: false,
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production'),
+            },
+        }),
+    ],
+};
+
+module.exports = config;
