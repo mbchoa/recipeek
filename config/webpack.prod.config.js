5f7982ab2e553992f3901801385991d76fe6661d
Michael-Bryant Choa <mbchoa@gmail.com>
2016-12-02 08:45:55 -0600

    Fix output path of frontend bundle file

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -7,7 +7,7 @@ module.exports = {
         './index.js',
     ],
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, '..', 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist/',
     },
