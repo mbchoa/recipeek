d9a0f6b0eae761876032518aa166c9345826db79
Michael Choa <mbchoa@gmail.com>
2017-01-11 02:27:30 -0800

    Dockerize App (#50)
    * Dockerize App
    
    * Separate backend server into separate API and Express apps

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ const path = require('path');
 const assign = require('lodash').assign;
 const nodeExternals = require('webpack-node-externals');
 const WebpackDevServer = require('webpack-dev-server');
-
 const frontWebpackConfig = require('./config/webpack.prod.config.js');
+
+// load dev environment variables
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const baseConfig = {
   module: {
@@ -22,11 +26,14 @@ const baseConfig = {
 const config = (overrides) => assign({}, baseConfig, overrides || {});
 
 const frontEndConfig = config(frontWebpackConfig);
-const backEndConfig = config({
-  entry: './server/apiServer.js',
+const backendConfig = config({
+  entry: './server/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'apiServer.js',
+    filename: 'server.bundle.js',
+    library: 'backend',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   target: 'node',
   node: {
@@ -49,27 +56,25 @@ const backEndConfig = config({
   ],
 });
 
-const onBuildComplete = done => (err, stats) => {
-  if (err) {
-    console.log('Error building', err);
-    throw err;
-  } else {
-    console.log(stats.toString());
-  }
-
-  if (done) done();
-};
-
 // production gulp tasks
 gulp.task('frontend-build', done => {
   webpack(frontEndConfig).run(onBuildComplete(done));
 });
 
 gulp.task('backend-build', done => {
-  webpack(backEndConfig).run(onBuildComplete(done));
+  webpack(backendConfig).run(onBuildComplete(done));
 });
 
-gulp.task('build', ['backend-build', 'frontend-build']);
+gulp.task('build', ['backend-build', 'frontend-build'], done => {
+  // launch app and api servers
+  const {
+    createApiServer,
+    createAppServer,
+  } = require('./dist/server.bundle.js');
+
+  createApiServer(process.env.PORT-1);
+  createAppServer(process.env.PORT);
+});
 
 // development gulp tasks
 const frontEndDevConfig = require('./config/webpack.dev.config.js');
@@ -84,16 +89,32 @@ gulp.task('frontend-dev', ['frontend-dev-assets', 'backend-build'], done => {
     publicPath: frontEndDevConfig.output.publicPath,
     hot: true,
     historyApiFallback: true,
-    proxy: {
-        "/api/*": "http://localhost:4000", // <-- Proxy /api/search/:ingredient to http://localhost:3000/api/search/:ingredient
-    }
   }).listen(8080, 'localhost', function (err, result) {
       if (err) return console.log(err);
       console.log('WDS listening on http://localhost:8080');
 
-      // start express web + api server
-      require('./dist/apiServer.js');
+      // start express api + app servers
+      const { 
+        createApiServer,
+        createAppServer,
+      } = require('./dist/server.bundle.js');
+
+      createApiServer(process.env.PORT-1);
+      createAppServer(process.env.PORT);
   });
 });
 
 gulp.task('dev', ['frontend-dev-assets', 'backend-build', 'frontend-dev']);
+
+function onBuildComplete (done) {
+  return function handleComplete (err, stats) {
+    if (err) {
+      console.log('Error building', err);
+      throw err;
+    } else {
+      console.log(stats.toString());
+    }
+
+    if (done) done();
+  }
+}
