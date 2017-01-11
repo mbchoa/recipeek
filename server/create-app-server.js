259cb2e2fef4ec01567e983444512e5b5dbb11e5
Michael-Bryant Choa <michael-bryant.choa@hautelook.com>
2017-01-10 21:24:53 -0800

    Separate backend server into separate API and Express apps

---
+++ server/create-app-server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import request from 'request';
+
+export default function startAppServer(port) {
+  const app = express();
+  app.use(express.static(__dirname));
+  app.use('/api/*', (req, res) => {
+    const proxyPath = `http://localhost:${port-1}${req.originalUrl}`;
+    request(proxyPath).pipe(res);
+  });
+
+  app.listen(port, function () {
+    console.log(`Recipeek express server listening on port ${this.address().port} in ${process.env.NODE_ENV} mode`);
+  });
+};
---
+++ server/index.js
@@ -0,0 +1,8 @@
+export { default as createApiServer } from './create-api-server';
+export { default as createAppServer } from './create-app-server';
+
+// import createApiServer from './create-api-server';
+// import createAppServer from './create-app-server';
+
+// createApiServer(process.env.PORT-1);
+// createAppServer(process.env.PORT);
