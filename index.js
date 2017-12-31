f0e450cd6e7ab725bc52b830ff479cc590776f95
Michael-Bryant Choa <mbchoa@gmail.com>
2018-01-01 07:53:58 +0900

    Setup Apollo Client

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import App from './components/App';
 
@@ -9,13 +13,19 @@ import configureStore from './redux/store/configureStore';
 import initialState from './redux/store/initialState';
 
 const store = configureStore(initialState);
+const client = new ApolloClient({
+  link: new HttpLink({ uri: `http://localhost:${process.env.PORT-1}/graphql` }),
+  cache: new InMemoryCache()
+});
 
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Provider store={store}>
-        <Component />
-      </Provider>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <Component />
+        </Provider>
+      </ApolloProvider>
     </AppContainer>,
     document.getElementById('app')
   );
