847a37fafc15f7384499ae1f67338b75f3fd4529
Michael-Bryant Choa <mbchoa@gmail.com>
2018-01-06 10:54:45 +0800

    Update CORS origin on API server

--- server/create-api-server.js
+++ server/create-api-server.js
@@ -5,30 +5,30 @@ import cors from 'cors';
 
 import graphqlRouter from './graphql/router';
 
-const app = express();
-app.use(compression());
-app.use('*', cors({ origin: 'http://localhost:4000' }));
+export default function startApiServer(apiPort) {
+  const app = express();
+  app.use('*', cors({ origin: `${process.env.API_PROTOCOL}://${process.env.API_DOMAIN}:${process.env.PORT}` }));
+  app.use(compression());
 
-graphqlRouter(app);
+  graphqlRouter(app);
 
-app.get('/api/v2/search/:ingredient', (req, res) => {
-  request(`https://api.edamam.com/search?q=${req.params.ingredient}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`, (err, resp, payload) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.set({
-      'Cache-Control': 'public, max-age=86400',
-      'Content-Type': 'application/json',
-    });
-    res.send({
-      status: 200,
-      data: JSON.parse(payload),
+  app.get('/api/v2/search/:ingredient', (req, res) => {
+    request(`https://api.edamam.com/search?q=${req.params.ingredient}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`, (err, resp, payload) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.set({
+        'Cache-Control': 'public, max-age=86400',
+        'Content-Type': 'application/json',
+      });
+      res.send({
+        status: 200,
+        data: JSON.parse(payload),
+      });
     });
   });
-});
 
-export default function startApiServer(port) {
-  app.listen(port, function listen() {
+  app.listen(apiPort, function listen() {
     console.log(`Express API server listening on port ${this.address().port} in ${process.env.NODE_ENV} mode`);
   });
 };
