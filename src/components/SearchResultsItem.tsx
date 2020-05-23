1543ff0e50a5576241f99e3b5d79d3b3957c2198
Michael-Bryant Choa <mbchoa@gmail.com>
2020-05-22 17:41:57 -0700

    Update styling of recipe item into a card component

--- src/components/SearchResultsItem.tsx
+++ src/components/SearchResultsItem.tsx
@@ -4,11 +4,19 @@ import styled from 'styled-components';
 import { EdamamRecipe } from '../types/edamam';
 
 const Block = styled.article`
-  max-width: 300px;
+  border: 1px solid #dcdcdc;
+  border-radius: 5px;
+  box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.4);
+  transition: all 0.2s ease-in-out;
+  :hover {
+    border-color: #acacac;
+    transform: scale(1.01);
+  }
 `;
 
 const Header = styled.h2`
   font-size: 18px;
+  margin: 16px;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
