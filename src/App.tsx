ea69110f82e4a2a2dad4abcfefaab39e066ac210
Michael-Bryant Choa <mbchoa@gmail.com>
2020-05-24 00:20:17 -0700

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import styled from 'styled-components';
 
 import store from './redux/store';
 
+import Footer from './components/Footer';
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
 import SearchResults from './components/SearchResults';
 
+const Main = styled.main`
+  flex: 1 0 auto;
+`;
+
 const App = () => (
   <Provider store={store}>
     <Header />
-    <main>
+    <Main>
       <SearchForm />
       <SearchResults />
-    </main>
+    </Main>
+    <Footer />
   </Provider>
 );
 
