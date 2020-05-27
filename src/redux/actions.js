1be0a5d461c6270f96d81339ec1a730d5c78a674
Michael-Bryant Choa <mbchoa@gmail.com>
2020-05-27 15:05:45 -0700

    Add fetchMoreRecipes action creators

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -11,3 +11,15 @@ export const searchSuccessful = fsaPayload(SEARCH_SUCCESSFUL);
 
 export const SEARCH_FAILURE = 'search failure';
 export const searchFailure = fsa(SEARCH_FAILURE);
+
+export const FETCH_MORE_RECIPES = 'fetch more recipes';
+export const fetchMoreRecipes = fsa(FETCH_MORE_RECIPES);
+
+export const FETCH_MORE_RECIPES_PENDING = 'fetch more recipes pending';
+export const fetchMoreRecipesPending = fsa(FETCH_MORE_RECIPES_PENDING);
+
+export const FETCH_MORE_RECIPES_SUCCESSFUL = 'fetch more recipes successful';
+export const fetchMoreRecipesSuccessful = fsa(FETCH_MORE_RECIPES_SUCCESSFUL);
+
+export const FETCH_MORE_RECIPES_FAILURE = 'fetch more recipes failure';
+export const fetchMoreRecipesFailure = fsa(FETCH_MORE_RECIPES_FAILURE);
