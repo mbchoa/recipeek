c0d04334e33080cbe1dc885802e3e5a89417f9e5
Michael-Bryant Choa <mbchoa@gmail.com>
2020-07-11 23:59:19 -0700

    refactor: Add link to header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { space } from '../enums/space';
+
 const StyledHeader = styled.header`
   align-items: center;
   background-color: #113f67;
-  box-sizing: border-box;
   display: flex;
-  height: 60px;
-  padding: 0 16px;
+  padding: ${space['sp-16']};
 `;
 
-const Title = styled.h1`
+const Link = styled.a`
+  text-decoration: none;
+`;
+
+const Heading = styled.h1`
   color: white;
   font-size: 20px;
   font-weight: 600;
+  margin: 0;
 `;
 
 const Header = () => (
   <StyledHeader>
-    <Title>recipeek</Title>
+    <Link href="/">
+      <Heading>recipeek</Heading>
+    </Link>
   </StyledHeader>
 );
 
