abf973b9e2dc5896cecdecb3158d0d6a8c0a37e4
Michael-Bryant Choa <mbchoa@gmail.com>
2021-04-10 17:56:03 -0700

    refactor: Update store config to only allow Redux DevTools in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers';
 import sagas from './sagas';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 sagaMiddleware.run(sagas);
 
